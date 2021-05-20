c58f253b035a44d8d6e3a7625f6ec20173a76e68
Vitaliy Grabchuk <vitaliygr23@gmail.com>
2021-05-19 18:09:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchButton = $("#searchBtn");
+var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
+
+// Get access to the OpenWeather API
+var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
